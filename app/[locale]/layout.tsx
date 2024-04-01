b306b8d61ff0cf727ca8a941c239e70fe0014ca8
skishore23 <shimikeri.kishore@gmail.com>
2024-04-02 03:01:48 +0530

    GupShup

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "GupShup"
+const APP_DEFAULT_TITLE = "GupShup"
+const APP_TITLE_TEMPLATE = "%s - GupShup"
+const APP_DESCRIPTION = "GupShup with AI!"
 
 interface RootLayoutProps {
   children: ReactNode
